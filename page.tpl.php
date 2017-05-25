126c56f35cdd3f78fd3c277f9de4a8f8dabeecf8
Yandong <yandong22222@gmail.com>
2017-05-25 16:54:53 +0800

    Accomplish html files from design.

---
+++ page.tpl.php
@@ -0,0 +1,140 @@
+<div class="header-wrap">
+  <header>
+    <a class="logo" href="#">
+      <img src="./sites/all/themes/open_charity/logo.png" height="100%" />
+    </a>
+    <ul class="main-nav">
+      <li><a href="#">ABOUT OPEN CHARITY</a></li>
+      <li><a href="#">THE BLOG</a></li>
+    </ul>
+    <a href="#" class="button">JOIN US</a>
+  </header>
+</div>
+<div class="banner">
+  <img src="./sites/all/themes/open_charity/images/banner1.png"/>
+  <div class="banner-text">
+    <h2>Sharing Ideas For Charities</h2>
+    <p>Many charities’ goals are similar, as is the functionality we require, but little shared working takes place.</p>
+    <p class="small">By working together, driving shared areas of interest and influencing open source developments we can bring efficiencies, improve the digital experience for our users, and have great impact.</p>
+    <p>Together we can make a bigger diference.</p>
+  </div>
+</div>
+<div class="next-event">
+  <a href="#" class="event">
+    <p><span class="tittle">Next Event: </span><span class="time">June 23rd 2016  18:30 - 21:00</span></p>
+    <p>Cancer Research UK, Angel Building, 407 St John Street, London EC1V 4AD</p>
+  </a>
+  <button class="button2">REGISTER</button>
+</div>
+<div class="get-involved-wrap">
+  <div class="get-involved">
+    <h2 class="region-title">GET INVOLVED</h2>
+    <div class="column3">
+      <dl>
+        <dt><img src="./sites/all/themes/open_charity/images/Meetup.png"/></dt>
+        <dd>WE DO MEETINGS</dd>
+        <dd>We organise our meetings through the OpenCharity MeetUp group</dd>
+        <dd><button>MEETUP GROUP</button></dd>
+      </dl>
+      <dl>
+        <dt><img src="./sites/all/themes/open_charity/images/Slack.png"/></dt>
+        <dd>WE COLLABORATE</dd>
+        <dd>OpenCharity have a slack group for daily collaboration</dd>
+        <dd><button>SLACK GROUP</button></dd>
+      </dl>
+      <dl>
+        <dt><img src="./sites/all/themes/open_charity/images/Share.png"/></dt>
+        <dd>WE SHARE</dd>
+        <dd>We have a Google Group set up to share tools and documents</dd>
+        <dd><button>GOOGLE GROUP</button></dd>
+      </dl>
+    </div>
+  </div>
+</div>
+<div class="our-mission">
+  <h2 class="region-title">OUR MISSION</h2>
+  <p class="declare">Charities and Partners collaborating and sharing open solutions and ideas to create value in the digital space.</p>
+  <p class="declare2">If you are a charity or a supplier, we are ready to welcome you.</p>
+  <div class="column3">
+    <dl>
+      <dt><img src="./sites/all/themes/open_charity/images/Bulb.png"/></dt>
+      <dd>We help charities</dd>
+      <dd>share knowledge and working practice to make the best technology choices.</dd>
+    </dl>
+    <dl>
+      <dt><img src="./sites/all/themes/open_charity/images/Group.png"/></dt>
+      <dd>We bring together</dd>
+      <dd>charities and suppliers to the charity sector to share best practices.</dd>
+    </dl>
+    <dl>
+      <dt><img src="./sites/all/themes/open_charity/images/Like.png"/></dt>
+      <dd>We encourage</dd>
+      <dd>collaboration and innovation for the good of the charity sector.</dd>
+    </dl>
+  </div>
+  <hr class="hr1"/>
+</div>
+<div class="our-members">
+  <h2 class="region-title">OUR MEMBERS</h2>
+  <div class="slider">
+    <div><img src="./sites/all/themes/open_charity/images/Cancer Research.png"/></div>
+    <div><img src="./sites/all/themes/open_charity/images/cc-standard-full-colour"/></div>
+    <div><img src="./sites/all/themes/open_charity/images/KopLogo.png"/></div>
+    <div><img src="./sites/all/themes/open_charity/images/ZingLogo.png"/></div>
+    <div><img src="./sites/all/themes/open_charity/images/ComicRelief.png"/></div>
+  </div>
+  <div class="slider-pages">
+    <span class="current"></span>
+    <span></span>
+    <span></span>
+  </div>
+</div>
+<div class="blogs">
+  <h2 class="region-title">BLOG</h2>
+  <div class="column4">
+    <a href="#">
+      <ul>
+        <li class="tittle">Online Donations Special</li>
+        <li class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </li>
+        <li class="time">14 Nov 2014</li>
+      </ul>
+    </a>
+    <a href="#">
+      <ul>
+        <li class="tittle">Online Donations Special </li>
+        <li class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </li>
+        <li class="time">14 Nov 2014</li>
+      </ul>
+    </a>
+    <a href="#">
+      <ul>
+        <li class="tittle">Online Donations Special</li>
+        <li class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </li>
+        <li class="time">14 Nov 2014</li>
+      </ul>
+    </a>
+    <a href="#">
+      <ul>
+        <li class="tittle">Online Donations Special</li>
+        <li class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </li>
+        <li class="time">14 Nov 2014</li>
+      </ul>
+    </a>
+  </div>
+  <span class="prev"><i class="fa fa-chevron-left"></i></span>
+  <span class="next"><i class="fa fa-chevron-right"></i></span>
+</div>
+<div class="blogs-bg">
+  <a href="#"></a>
+</div>
+<div class="footer-wrap">
+  <footer>
+    <div class="social-media">
+      <a href="#" class="fa fa-facebook" aria-hidden="true"></a>
+      <a href="#" class="fa fa-twitter" aria-hidden="true"></a>
+      <a href="#" class="fa fa-google-plus" aria-hidden="true"></a>
+    </div>
+    <p class="build-by">This site was built as a collaboration between <a href="#">Manifesto Digital</a> and <a href="#">Compucorp</a></p>
+  </footer>
+</div>
+<!--<?php print render($page['content']); ?>-->
